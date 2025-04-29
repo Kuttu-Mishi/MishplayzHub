16aa902eb9938b7387930b6012146fe728692d4b
Kuttu-Mishi <140500633+Kuttu-Mishi@users.noreply.github.com>
2025-04-29 06:31:24 +0000

    updated pong

--- Pong.js
+++ Pong.js
@@ -6,7 +6,7 @@ const keys = {};
 const p1 = {
   x: 5,
   y: 300,
-  width: 0,
+  width: 10,
   height: 50,
   color: "red",
   speed: 10
@@ -63,7 +63,27 @@ function drawBall() {
   ctx.stroke();
 }
 
-drawp1();
-drawp2();
+function drawScore() {
+  ctx.font = "20px Arial";
+  ctx.fillStyle = "white";
+  ctx.fillText("player 1: " + p1.score, 10, 20);
+  ctx.fillText("player 2: " + p2.score, canvas.width - 100, 20);
+}
 
+function loop() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  if (keys["w"] && p1.y > 0) p1.y -= p1.speed;
+  if (keys["s"] && p1.y < canvas.height - p1.height) p1.y += p1.speed;
+
+  if (keys["ArrowUp"] && p2.y > 0) p2.y -= p2.speed;
+  if (keys["ArrowDown"] && p2.y < canvas.height - p2.height) p2.y += p2.speed;
+
+  drawp1();
+  drawp2();
+  drawBall();
+  drawScore();
+
+  requestAnimationFrame(loop);
+}
 
